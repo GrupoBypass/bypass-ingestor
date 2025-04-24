8e113daee673622126df3bad68ee403a31875145
gba-exe <125586278+gba-exe@users.noreply.github.com>
2025-04-23 21:16:41 -0300

    feat(azure): conectei o sistema ate o ambiente nuvem

---
+++ tests/run_test_iot_interface.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from cloud.iot_interface import AzureIoTInterface
+
+
+def generate_data():
+    return {
+        "dataHora": ["2023-10-01 12:00:00", "2023-10-01 12:05:00"],
+        "statusDPS": ["OK", "FALHA"],
+        "picoTensao_kV": [0.0, 5.0],
+        "correnteSurto_kA": [0.0, 10.0],
+    }
+
+
+if __name__ == "__main__":
+    connection_string = os.getenv("AZURE_IOT_CONNECTION_STRING")
+
+    iot_interface = AzureIoTInterface(connection_string=connection_string)
+
+    data = generate_data()
+
+    iot_interface.send_message(data)
