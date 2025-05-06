d948aeedc0f1895e30c598f78c3dad428ef52e01
babi-barbato <barbara.oliveira@sptech.school>
2025-05-06 17:57:35 -0300

    fix: função de gerar matriz do omron

--- sensors/omron_sensor.py
+++ sensors/omron_sensor.py
@@ -1,13 +1,41 @@
 import random
+import numpy as np
 from sensors.base_sensor import BaseSensor
 
 class SensorOmron(BaseSensor):
-    def __init__(self, x=50, y=50):
+    def __init__(self, hotspots, percent, noise, radius_min, radius_max, lines, columns):
         super().__init__("omron")
-        self.x = x
-        self.y = y
+        self.lines = lines
+        self.columns = columns
+        self.hotspots = hotspots
+        self.percent = percent
+        self.noise = noise
+        self.radius_min = radius_min 
+        self.radius_max = radius_max 
 
     def generate_matrix(self):
-        """Gera uma matriz x por y com valores 0 ou 1"""
-        matriz = [[random.randint(0, 1) for _ in range(self.y)] for _ in range(self.x)]
+        matriz = np.zeros((self.lines, self.columns))
+
+        for _ in range(self.hotspots):
+            centro_x = random.randint(3, self.lines - 4)
+            centro_y = random.randint(3, self.columns - 4)
+            raio = random.randint(self.radius_min, self.radius_max)
+
+
+            for i in range(centro_x - raio, centro_x + raio + 1):
+                for j in range(centro_y - raio, centro_y + raio + 1):
+                    if 0 <= i < self.lines and 0 <= j < self.columns:
+                        if random.random() < self.percent:
+                            matriz[i][j] = 1
+
+
+        for i in range(self.lines):
+            for j in range(self.columns):
+                if matriz[i][j] == 0 and random.random() < self.noise:
+                    matriz[i][j] = 1
+
+
+        porcentagem_ativos = np.sum(matriz) / matriz.size
+        # print(f"Porcentagem de áreas ativas: {porcentagem_ativos:.2%}")
+
         return matriz
