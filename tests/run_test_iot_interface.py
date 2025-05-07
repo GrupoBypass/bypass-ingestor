333aebe412bb7f1550db6069802dafced538e8a7
babi-barbato <barbara.oliveira@sptech.school>
2025-05-07 01:22:41 -0300

    A

--- tests/run_test_iot_interface.py
+++ tests/run_test_iot_interface.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
     # connection_string = "HostName=barbara02231047.azure-devices.net;DeviceId=barbara-02231047;SharedAccessKey=pnlb6SNJAah/pOQ7Nq27tmqmhzzB5dclRaqbBRof1pY="
     connection_string = "HostName=barbara02231047.azure-devices.net;DeviceId=gabriel-device;SharedAccessKey=dmavQPyvC1JQmqhRcWjbB/ZhiCeBFbWCW0l2HNRTEBU="
 
-
     iot_interface = AzureIoTInterface(connection_string=connection_string)
 
     data = generate_data()
+    print(data)
 
     iot_interface.send_message(data)
