aecf3be09f0a64288636e3f4f73c19e435ea45e6
babi-barbato <barbara.oliveira@sptech.school>
2025-05-18 22:50:25 -0300

    fix(): Ajustes de envio pra nuvem

--- sensors/omron_sensor.py
+++ sensors/omron_sensor.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import pandas as pd
 from sensors.base_sensor import BaseSensor
 
 class SensorOmron(BaseSensor):
@@ -21,21 +22,25 @@ class SensorOmron(BaseSensor):
             centro_y = random.randint(3, self.columns - 4)
             raio = random.randint(self.radius_min, self.radius_max)
 
-
             for i in range(centro_x - raio, centro_x + raio + 1):
                 for j in range(centro_y - raio, centro_y + raio + 1):
                     if 0 <= i < self.lines and 0 <= j < self.columns:
                         if random.random() < self.percent:
                             matriz[i][j] = 1
 
-
         for i in range(self.lines):
             for j in range(self.columns):
                 if matriz[i][j] == 0 and random.random() < self.noise:
                     matriz[i][j] = 1
 
-
         porcentagem_ativos = np.sum(matriz) / matriz.size
         # print(f"Porcentagem de áreas ativas: {porcentagem_ativos:.2%}")
 
-        return matriz
+        # Conversão para DataFrame com colunas x1, x2... e índice y1, y2...
+        df = pd.DataFrame(
+            matriz,
+            columns=[f"x{i+1}" for i in range(self.columns)],
+            index=[f"y{j+1}" for j in range(self.lines)]
+        )
+        
+        return df
