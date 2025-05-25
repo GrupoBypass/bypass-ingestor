187d59f6cb704f28b501a331b13310836858ad4d
babi-barbato <barbara.oliveira@sptech.school>
2025-05-25 13:08:20 -0300

    remove(): Sensor gps

--- process/distance.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def calculate_distance(point_a, point_b):
-    # Utilizada no sensor de GPS
-    return math.sqrt(
-        (point_b[0] - point_a[0]) ** 2
-        + (point_b[1] - point_a[1]) ** 2
-        + (point_b[2] - point_a[2]) ** 2
-    )
