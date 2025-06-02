4da5896c66338e836109aacd7ddf3e8bf57af0d8
babi-barbato <barbara.oliveira@sptech.school>
2025-06-01 22:48:28 -0300

    refactor(): Função de gerar dados do omron

--- process/omron_processor.py
+++ process/omron_processor.py
@@ -1,15 +1,9 @@
-import pandas as pd
 from sensors.omron_sensor import SensorOmron
-import json
 
 class OmronProcessor:
-    def __init__(self, hotspots, percent, noise, radius_min, radius_max, lines, columns):
-        self.sensor = SensorOmron(hotspots, percent, noise, radius_min, radius_max, lines, columns, seed=46)
+    def __init__(self, hotspots, percent, noise,radius_min, radius_max, lines, columns, porta_hotspots):
+        self.sensor = SensorOmron(hotspots, percent, noise,radius_min, radius_max, lines, columns, porta_hotspots)
 
     def generate_data_list(self) -> list:
-        matriz = self.sensor.generate_matrix()
-        
-        json_str = matriz.to_json(orient='records', date_format='iso')
-        json_obj = json.loads(json_str)
-        
-        return json_obj
+        lista = self.sensor.generate_matrix()
+        return lista
